4eae481920850d3c99a8fcd724c56624775657e0
lanson647 <lanson647@163.com>
2014-09-29 15:54:47 +0800

    v4
    v4

--- js/window.js
+++ js/window.js
@@ -1,7 +1,12 @@
 define(["jquery"],function($){
-	function Window(){};
+	function Window(){
+		this.cfg = {
+			width: 500,
+			height: 300
+		};
+	};
 	Window.prototype = {
-		alert: function(content,handler){
+		alert: function(content,handler,cfg){
 			var boundingBox = $('<div class="window_boundingBox"></div>');
 			var btn  = $('<input type="button" value="确定" />');
 			boundingBox.html(content);
@@ -11,7 +16,14 @@ define(["jquery"],function($){
 				handler && handler();
 				boundingBox.remove();
 			});
-		},
+			$.extend(this.cfg,cfg);
+			boundingBox.css({
+				width: this.cfg.width + "px",
+				height: this.cfg.height + "px",
+				left: (this.cfg.x || (window.innerWidth - this.cfg.width) / 2) + "px",
+				top: (this.cfg.y || (window.innerHeight - this.cfg.height) / 2) + "px"
+			});
+		},//end alert
 		confirm:function(){
 
 		},
